ebbf7843f3c31c4b0761a4950c8c407f8c62d308
miguel amador <Miguelamador23@outlook.com>
2023-11-13 10:58:19 -0600

    final1

--- code/modo_oscuro.js
+++ code/modo_oscuro.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const body = document.body;
 
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        body.classList.add('dark-mode');
-    }
-
-    darkModeToggle.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-        localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
-    });
-
-  
+   
     const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+
     if (isDarkMode) {
         body.classList.add('dark-mode');
     }
+
+    darkModeToggle.addEventListener('click', function () {
+      
+        body.classList.toggle('dark-mode');
+
+        localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
+    });
 });
