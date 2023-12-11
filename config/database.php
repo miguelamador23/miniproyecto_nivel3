560d729931217aac2d1aea3bdce18cf94dd86190
¨miguelamador23¨ <miguelamador23@outlook.com>
2023-12-11 10:30:42 -0600

    versionfinal2

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ try {
     $db_username = "root";
     $db_password = "";
     $db_name = "login_db";
-    $db_port = 3307;
+    $db_port = 3306;
 
     $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name, $db_port);
 
